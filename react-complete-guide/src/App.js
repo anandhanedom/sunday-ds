3378ad6d9d8cb7361801822cdad1d61efb2c35b1
Anandhan <anandhanedom@gmail.com>
2020-10-14 19:43:27 +0530

    learning

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+//Components
+import Person from './components/person/person.component.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Person />
+    </div>
+  );
+}
+
+export default App;
