5f990bdfcac941e9d303a395d4a05fd03a19c337
Anandhan <anandhanedom@gmail.com>
2020-10-15 12:48:05 +0530

    essentials

--- react-complete-guide/src/components/person/person.component.jsx
+++ react-complete-guide/src/components/person/person.component.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const Person = () => {
-  return <h1>I'm a person</h1>;
+const Person = ({ name, age, children, click }) => {
+  return (
+    <div>
+      <p onClick={click}>
+        I'm {name} and I'm {age} years old!
+      </p>
+      <p>{children}</p>
+    </div>
+  );
 };
 
 export default Person;
