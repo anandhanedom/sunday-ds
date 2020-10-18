e4ec1441bbab172a85469ba1da25239a151855e4
Anandhan <anandhanedom@gmail.com>
2020-10-18 20:44:57 +0530

    layout component

--- 00001_burger_builder/src/App.js
+++ 00001_burger_builder/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import Layout from './components/Layout/Layout';
+
 function App() {
-  return <div></div>;
+  return (
+    <Layout>
+      <p>Test</p>
+    </Layout>
+  );
 }
 
 export default App;
