b86e32285c7bb4db859dca1f1722a69b691ac96d
Anandhan <anandhanedom@gmail.com>
2020-10-26 23:10:53 +0530

    pg completed

--- redux-async-demo/asyncActions.js
+++ redux-async-demo/asyncActions.js
@@ -1,8 +1,8 @@
 const redux = require('redux');
-const createStore = redux.createStore;
-const applyMiddleware = redux.applyMiddleware;
 const thunkMiddleware = require('redux-thunk').default;
 const axios = require('axios');
+const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
 
 const initialState = {
   loading: false,
@@ -27,42 +27,55 @@ const fetchUsersSuccess = (users) => {
   };
 };
 
-const fetchUsersFailure = (err) => {
+const fetchUsersFailure = (error) => {
   return {
     type: FETCH_USERS_FAILURE,
-    payload: err,
+    payload: error,
+  };
+};
+
+const fetchUsers = () => {
+  return function (dispatch) {
+    dispatch(fetchUsersRequest());
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        // response.data is the users
+        const users = response.data.map((user) => user.id);
+        dispatch(fetchUsersSuccess(users));
+      })
+      .catch((error) => {
+        // error.message is the error message
+        dispatch(fetchUsersFailure(error.message));
+      });
   };
 };
 
 const reducer = (state = initialState, action) => {
+  console.log(action.type);
   switch (action.type) {
     case FETCH_USERS_REQUEST:
       return {
         ...state,
         loading: true,
       };
-
     case FETCH_USERS_SUCCESS:
       return {
-        ...state,
         loading: false,
         users: action.payload,
         error: '',
       };
-
     case FETCH_USERS_FAILURE:
       return {
-        ...state,
         loading: false,
         users: [],
         error: action.payload,
       };
-
-    default:
-      return state;
   }
 };
 
-const fetchUsers = () => {};
-
 const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch(fetchUsers());
