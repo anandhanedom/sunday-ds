af222fdb2ba31c15cddcd061cbaf59c7dd96fd33
Anandhan <anandhanedom@gmail.com>
2020-10-26 15:22:37 +0530

    redux foundation

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,1 +1,40 @@
-console.log('From index.js');
+const redux = require('redux');
+
+const createStore = redux.createStore();
+
+const BUY_CAKE = 'BUY_CAKE';
+
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: 'First redux action',
+  };
+}
+
+// (prevState,action)=>newState
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+//Redux store
+const store = createStore(reducer);
+
+//Get current state
+console.log('Initial state', store.getState());
+
+//Subscribe for changes
+store.subscribe(() => console.log('Updated state', store.getState()));
