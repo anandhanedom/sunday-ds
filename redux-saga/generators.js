9fce414274ed679381ad6128ddd075c3d55a87a1
Anandhan <anandhanedom@gmail.com>
2020-11-20 23:09:04 +0530

    gens

--- redux-saga/generators.js
+++ redux-saga/generators.js
@@ -10,14 +10,33 @@ function* sayHiGen(params) {
 
 const resGen = sayHiGen();
 
+//Gen object
 console.log(resGen);
 
+//{value,done}
 console.log(resGen.next());
 console.log(resGen.next());
 console.log(resGen.next());
 
 const resGenForOf = sayHiGen();
 
+//Looping yield values
 for (const i of resGenForOf) {
   console.log(i);
 }
+
+function* newGenerator() {
+  yield 'something';
+
+  const final = yield 'give me the value';
+
+  //   return 'final value';
+  return final;
+}
+
+const newGen = newGenerator();
+
+console.log('*********************');
+console.log(newGen.next());
+console.log(newGen.next());
+console.log(newGen.next('custom value'));
