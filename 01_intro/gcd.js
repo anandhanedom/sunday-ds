018e9c982e3f062e85957facc4cef8ec2f226ec2
Anandhan Sreekumar <anandhanedom@gmail.com>
2022-02-13 15:32:32 +0530

    feat: todays code

--- 01_intro/gcd.js
+++ 01_intro/gcd.js
@@ -18,16 +18,35 @@ const getGCD = (a, b) => {
 // console.log(newGCD);
 
 const calculateGCDWithEuclid = (a, b) => {
-  if (b === 0) {
-    return a;
-  } else if (a > b) {
-    calculateGCDWithEuclid(a - b, b);
-  } else {
-    calculateGCDWithEuclid(a, b - a);
+  if (a === 0) return b;
+
+  if (b) {
+    if (a > b) {
+      calculateGCDWithEuclid(a - b, b);
+    } else {
+      calculateGCDWithEuclid(a, b - a);
+    }
   }
+
+  return a;
 };
 
-const newGCD = calculateGCDWithEuclid(5, 20);
+// const calculateGCDWithEuclid = (a, b) => {
+//   if (a === 0) return b;
+//   if (b === 0) return a;
+
+//   while (b) {
+//     if (a > b) {
+//       a = a - b;
+//     } else {
+//       b = b - a;
+//     }
+//   }
+
+//   return a;
+// };
+
+const newGCD = calculateGCDWithEuclid(20, 0);
 console.log(newGCD);
 
 // const calculateGCDWithEuclid = (a, b) => {
@@ -36,3 +55,11 @@ console.log(newGCD);
 
 // const newGCD = calculateGCDWithEuclid(0, 20);
 // console.log(newGCD);
+
+// Robots
+
+// 1. lex,loy,mig,rae
+// 2. marv, rae, lex
+// 3. lex marv rae ty
+// 4. lex marv ty rae
+// 5. lex rae mig marv ty
